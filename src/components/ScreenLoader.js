ecaa56ba275fe996071ea88f5b52723a1eaf3254
Muhammad Nouman <140593593+NoumanBunty@users.noreply.github.com>
2024-08-17 15:28:39 +0500

    Delete src directory

--- src/components/ScreenLoader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ScreenLoader() {
-    return (
-        <div className='loader-container'>
-            <span className="loader"></span>
-        </div>
-    )
-}
